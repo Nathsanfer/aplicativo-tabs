afd1aa2f499d791d11c95a728a1f64a80d82a6ed
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-03-13 14:14:58 -0300

    feat: add icons

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,40 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 export default function Layout() {
     return (
-        <Tabs>
-            <Tabs.Screen name="index" options={{ title: "Home" }} />
-            <Tabs.Screen name="profile" options={{ title: "Perfil" }} />
-            <Tabs.Screen name="news" options={{ title: "Notícias" }} />
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: "blue",
+                tabBarInactiveTintColor: "gray",
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Perfil",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="person" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="news"
+                options={{
+                    title: "Notícias",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="book" color={color} size={size} />
+                    ),
+                }}
+            />
         </Tabs>
     );
 }
